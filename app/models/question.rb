8828a817701faa6d93c3f61ef78a35d6f542b924
haruka kinami <pooh.pooh.po0oh.38@gmail.com>
2025-06-15 16:15:26 +0900

    fix:Lintチェックエラー

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   validates :body, presence: true, length: { maximum: 65_535 }
   validates :nickname, length: { maximum: 32 }, allow_blank: true
-  validates :age, numericality: {only_integer: true}, allow_blank: true
+  validates :age, numericality: { only_integer: true }, allow_blank: true
 end
