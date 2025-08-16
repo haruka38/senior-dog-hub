6575597a93acd7e7588e1777fc748d98d1ac46ee
haruka kinami <pooh.pooh.po0oh.38@gmail.com>
2025-08-16 22:28:19 +0900

    add:質問の編集と削除

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,9 @@ class QuestionsController < ApplicationController
 
   before_action :set_question_stamp_types
   before_action :set_answer_stamp_types
+  before_action :set_question, only: [ :show, :edit, :update, :destroy ]
+  before_action :authenticate_user_or_guest, only: [ :edit, :update, :destroy ] # ユーザーがログインしているかの確認
+  before_action :correct_user_or_guest?, only: [ :edit, :update, :destroy ] # ユーザーが投稿者かどうかの確認
 
   def index
     @questions = Question.all
@@ -16,6 +19,12 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
 
+    if user_signed_in?
+      @question.user = current_user
+    else
+      @question.guest_id = cookies[:guest_id]
+    end
+
     if @question.save
       redirect_to questions_path
     else
@@ -24,13 +33,32 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
-    # idの部分にはURLのquestionの数字部分が入る
     @question_answer = QuestionAnswer.new
     @question_answers = @question.question_answers
   end
 
+  def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question, notice: "質問を更新しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @qustion.destroy
+    redirect_to questions_path, notice: "質問を削除しました"
+  end
+
   private
+
+  def set_question
+    @question = Question.find(params[:id])
+    # idの部分にはURLのquestionの数字部分が入る
+  end
 
   def question_params
     params.require(:question).permit(:nickname, :breed_id, :age, :body)
@@ -42,5 +70,14 @@ class QuestionsController < ApplicationController
 
   def set_answer_stamp_types
     @answer_stamp_types = StampType.where(id: ANSWER_STAMP_TYPE_IDS)
+  end
+
+  def correct_user_or_guest?
+    (current_user && @question.user == current_user) ||
+    (!current_user && @question.guest_id == cookies[:guest_id])
+  end
+
+  def authorize_user_or_guest
+    redirect_to questions_path, alert: "権限がありません" unless correct_user_or_guest?
   end
 end
